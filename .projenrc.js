9a1142f68fb42f15cbb7881788bc8781048b2689
Filip Pýrek <PyrekFilip@gmail.com>
2022-02-08 21:10:31 +0100

    chore: setup projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,63 @@
 const { cdk8s } = require('projen');
 const project = new cdk8s.ConstructLibraryCdk8s({
-  author: 'Filip Pýrek',
-  authorAddress: 'PyrekFilip@gmail.com',
-  cdk8sVersion: '1.4.10',
-  defaultReleaseBranch: 'main',
-  name: 'buttonize-cdk',
+  name: '@buttonize/cdk',
+  license: 'MIT',
+  minNodeVersion: '14.17.0',
+
+  author: 'Buttonzie',
+  authorOrganization: 'Buttonize',
+  authorUrl: 'https://buttonize.io',
+
+  defaultReleaseBranch: 'master',
   repositoryUrl: 'git@github.com:buttonize/buttonize-cdk.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  devDeps: ['@commitlint/cli@^16.0.0', '@commitlint/config-conventional@^16.0.0', 'husky@^7.0.4', 'lint-staged@^11.2.6'],
+
+  // catalog: {
+  // 	announce: true,
+  // 	twitter: '@buttonize'
+  // },
+
+  // Will be enabled when Buttonize is released
+  publishToMaven: false,
+  publishToGo: false,
+  publishToPypi: false,
+  publishToNuget: false,
+
+
+  // Maybe nice features, but keeping it simple for now
+  depsUpgrade: false,
+  stale: false,
+  mutableBuild: false,
+
+  jestOptions: {
+    jestConfig: {
+      collectCoverage: true,
+      collectCoverageFrom: ['src/**/*.ts'],
+      coverageProvider: 'v8',
+      coverageThreshold: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+  },
+
+  // Maybe nice features, but keeping it simple for now
+  githubOptions: {
+    mergify: false,
+    clobber: false,
+    pullRequestLint: false,
+  },
 });
+
+project.addFields({
+  'lint-staged': {
+    '*.{ts,js,json}': 'eslint --fix',
+  },
+});
+
+project.setScript('prepare', 'husky install');
+
 project.synth();
